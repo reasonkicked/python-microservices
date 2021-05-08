f9d5c678679dab526b0614e4187af546b6c1fb88
tstanislawczyk <tstanislawczyk@pgs-soft.com>
2021-05-08 11:27:55 +0200

    producer and consumer running 1:12 ok

--- main/consumer.py
+++ main/consumer.py
@@ -1,6 +1,8 @@
 # amqps://bfcstckj:oLB6aJ4IP2iwtHaS_M2Yqeza1FdR94v_@cow.rmq2.cloudamqp.com/bfcstckj
 import pika, json
 
+from main import Product, db
+
 params = pika.URLParameters('amqps://bfcstckj:oLB6aJ4IP2iwtHaS_M2Yqeza1FdR94v_@cow.rmq2.cloudamqp.com/bfcstckj')
 
 connection = pika.BlockingConnection(params)
@@ -12,10 +14,30 @@ channel.queue_declare(queue='main')
 
 def callback(ch, method, properties, body):
     print('Recieved in main')
-    print(body)
+    data = json.loads(body)
+    print(data)
+
+    if properties.content_type == 'product_created':
+        product = Product(id=data['id'], title=data['title'], image=data['image'])
+        db.session.add(product)
+        db.session.commit()
+        print('Product Created')
+
+    elif properties.content_type == 'product_updated':
+        product = Product.query.get(data['id'])
+        product.title = data['title']
+        product.image = data['image']
+        db.session.commit()
+        print('Product updated')
+
+    elif properties.content_type == 'product_deleted':
+        product = Product.query.get(data)
+        db.session.delete(product)
+        db.session.commit()
+        print('Product deleted')
 
 
-channel.basic_consume(queue='main', on_message_callback=callback)
+channel.basic_consume(queue='main', on_message_callback=callback, auto_ack=True)
 
 print('Started consuming')
 
