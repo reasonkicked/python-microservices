a932aed028689b97883aa48795adc5b5d11aeb10
tstanislawczyk <tstanislawczyk@pgs-soft.com>
2021-05-09 12:25:00 +0200

    2:08 working

--- react-crud/src/App.tsx
+++ react-crud/src/App.tsx
@@ -3,6 +3,8 @@ import './App.css';
 import Products from "./admin/Products";
 import {BrowserRouter, Route} from "react-router-dom";
 import Main from "./main/Main";
+import ProductsCreate from "./admin/ProductsCreate";
+import ProductsEdit from "./admin/ProductsEdit";
 
 function App() {
   return (
@@ -12,7 +14,10 @@ function App() {
 
                         <BrowserRouter>
                             <Route path='/' exact component={Main} />
-                            <Route path='/admin/products' component={Products}/>
+                            <Route path='/admin/products' exact component={Products}/>
+                            <Route path='/admin/products/create' exact component={ProductsCreate}/>
+                            <Route path='/admin/products/:id/edit' exact component={ProductsEdit}/>
+
                         </BrowserRouter>
 
 
--- react-crud/src/admin/Products.tsx
+++ react-crud/src/admin/Products.tsx
@@ -1,6 +1,7 @@
 import React, {useEffect, useState} from 'react';
 import Wrapper from './Wrapper';
 import {Product} from "../interfaces/product"
+import {Link} from "react-router-dom"
 
 
 const Products = () => {
@@ -19,10 +20,26 @@ const Products = () => {
         )();
     });
 
+
+    const del = async (id: number) => {
+        if (window.confirm('Are you sure you want to delete this product?')) {
+            await fetch(`http://localhost:8000/api/products/${id}`, {
+                    method: 'DELETE'
+            });
+
+            setProducts(products.filter((p: Product) => p.id !== id));
+        }
+    }
+
     return (
 
 
 <Wrapper>
+    <div className="pt-3 pb-2 mb-3 border-bottom">
+                <div className="btn-toolbar mb-2 mb-md-0">
+                    <Link to='/admin/products/create' className="btn btn-sm btn-outline-secondary">Add</Link>
+                </div>
+    </div>
     <div className="table-responsive">
         <table className="table table-striped table-sm">
           <thead>
@@ -42,8 +59,16 @@ const Products = () => {
                         <td>{p.id}</td>
                         <td><img src={p.image} height="180"/></td>
                         <td>{p.title}</td>
-                        <td>{p.title}</td>
-                        <td>text</td>
+                        <td>{p.likes}</td>
+                        <td>
+                            <div className="btn-group mr-2">
+                                <Link to={`/admin/products/${p.id}/edit`}
+                                    className="btn btn-sm btn-outline-secondary">Edit</Link>
+                                <a href="#" className="btn btn-sm btn-outline-secondary"
+                                    onClick={() => del(p.id)}
+                                >Delete</a>
+                            </div>
+                        </td>
                     </tr>
                 )
           })}
